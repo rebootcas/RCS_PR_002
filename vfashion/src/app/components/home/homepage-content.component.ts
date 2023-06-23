a38d3eb2460ffb54de6f6306f03b751c6379a961
Sableprashant96 <sableprashant96@gmail.com>
2023-06-23 22:38:57 +0530

    home page update

---
+++ vfashion/src/app/components/home/homepage-content.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-homepage-content',
+  templateUrl: './homepage-content.component.html',
+  styleUrls: ['./homepage-content.component.css']
+})
+export class HomepageContentComponent { }
